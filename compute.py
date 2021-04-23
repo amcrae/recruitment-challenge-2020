c7a686b08b6935234b7df5739426b8819356811d
Andrew McRae <ajmcrae@gmail.com>
2021-04-23 15:21:05 +1000

    Initial data wrangler to get April actuals. Req R2.

---
+++ compute.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 23 13:29:41 2021
+
+@author: andrew
+"""
+
+from data_wrangler import download_AEMO_month
+
+# Req R2.
+print( download_AEMO_month(2020,4) )
---
+++ data_wrangler.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 23 08:23:35 2021
+
+@author: andrew
+"""
+import os
+import requests
+import pandas as pd
+
+def download_AEMO_month(Y,M):
+    ys = str(Y)
+    ms = "%02d" % (M,)
+    monthcode = ys+ms
+    AUTH_COOKIE_URL = 'https://aemo.com.au/energy-systems/electricity/national-electricity-market-nem/data-nem/aggregated-data'
+    RECENT_DATA_URL = 'https://aemo.com.au/aemo/data/nem/priceanddemand/PRICE_AND_DEMAND_'+monthcode+'_QLD1.csv'
+    
+    browser_headers= [
+                {
+                    "name": "Accept",
+                    "value": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+                },
+                {
+                    "name": "Accept-Encoding",
+                    "value": "gzip, deflate, br"
+                },
+                {
+                    "name": "Accept-Language",
+                    "value": "en-US,en;q=0.5"
+                },
+                {
+                    "name": "Connection",
+                    "value": "keep-alive"
+                },
+                {
+                    "name": "DNT",
+                    "value": "1"
+                },
+                {
+                    "name": "Host",
+                    "value": "aemo.com.au"
+                },
+                {
+                    "name": "Referer",
+                    "value": "https://aemo.com.au/aemo/apps/visualisation/index.html"
+                },
+                {
+                    "name": "Upgrade-Insecure-Requests",
+                    "value": "1"
+                },
+                {
+                    "name": "User-Agent",
+                    "value": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0"
+                }
+    ]
+    new_headers={ m["name"]:m["value"] for m in browser_headers }
+    
+    cookiedResponse = requests.get(AUTH_COOKIE_URL, headers=new_headers)
+    cookieJar = cookiedResponse.cookies
+    print( repr(cookieJar) )
+    cookieJar.set("privacy-notification","1")
+    
+    folder= "data"
+    filename='QLD_demand_'+monthcode+'.csv'
+    downloadedRecentFile=folder+os.sep+filename
+    try: 
+        res = requests.get(RECENT_DATA_URL, headers=new_headers, cookies=cookieJar)
+        if 200 <= res.status_code <= 320:
+            print("Response is OK")
+        else:
+            raise IOError("Cannot download recent month data, HTTP status "+str(res.status_code) )
+        with open(downloadedRecentFile, 'wb') as fd:
+            for chunk in res.iter_content(chunk_size=4096):
+                fd.write(chunk)
+        print("downloaded finished without error.")
+        df = pd.read_csv(downloadedRecentFile, index_col="SETTLEMENTDATE")
+        if not ("TOTALDEMAND" in df.columns):
+            raise IOError("Unexpected data format in download.")
+        print("File format test OK.")
+    except:
+        raise IOError("Cannot download recent month data")
+
+    return {"Sucess":True, "filepath":downloadedRecentFile}
+
+
+if __name__ == "__main__":
+    download_AEMO_month(2020,4)
